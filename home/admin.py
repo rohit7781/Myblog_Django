01d05af43c7850bf5114d3c97307155ec15aa638
Rohit Kumar <rohitkumar703314@gmail.com>
2021-12-06 21:03:32 +0530

    Everythiing almost done

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from home.models import Contact,requestblog,Blog
 # Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    class Media:
+        css = {
+            "all":("css/main2.css",)
+        }
+        js = ("js/blog.js",)
+
 admin.site.register(Contact)
 admin.site.register(requestblog)
-admin.site.register(Blog)
+admin.site.register(Blog,BlogAdmin)
